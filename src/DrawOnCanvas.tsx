4b0c2ccffb12ccc6211c62b5540d6847557b317d
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-04-04 15:59:20 +0200

    updated types and added componentDidUpdate function.

--- src/DrawOnCanvas.tsx
+++ src/DrawOnCanvas.tsx
@@ -12,7 +12,7 @@ interface Props {
 export default class DrawOnCanvas extends Component<Props> {
     private div: HTMLDivElement | null
     private setDivRef: (instance: HTMLDivElement | null) => void
-    private draw: any
+    private draw?: Draw
 
     constructor(props: Props) {
         super(props);
@@ -32,20 +32,38 @@ export default class DrawOnCanvas extends Component<Props> {
         }
     }
 
+    componentDidUpdate(prevProps: Props) {
+        if (!this.draw) {
+            return;
+        }
+
+        if ((prevProps.backgroundColor !== this.props.backgroundColor) && this.props.backgroundColor) {
+            this.draw.changeBackgroundColor(this.props.backgroundColor);
+        }
+
+        if ((prevProps.strokeColor !== this.props.strokeColor) && this.props.strokeColor) {
+            this.draw.changeStrokeColor(this.props.strokeColor);
+        }
+
+        if ((prevProps.strokeWeight !== this.props.strokeWeight) && this.props.strokeWeight) {
+            this.draw.changeStrokeWeight(this.props.strokeWeight);
+        }
+      }
+
     changeStrokeColor(color: string) {
-        this.draw.changeStrokeColor(color)
+        this.draw?.changeStrokeColor(color)
     } 
 
     changeStrokeWeight(strokeWeight: number) {
-        this.draw.changeStrokeWeight(strokeWeight);
+        this.draw?.changeStrokeWeight(strokeWeight);
     }
 
     changeBackgroundColor(backgroundColor: string) {
-        this.draw.changeBackgroundColor(backgroundColor);
+        this.draw?.changeBackgroundColor(backgroundColor);
     }
 
     reset() {
-        this.draw.reset();
+        this.draw?.reset();
     }
 
     render() {
@@ -53,18 +71,18 @@ export default class DrawOnCanvas extends Component<Props> {
     };
 
     downloadPNG(filename?: string) {
-        this.draw.downloadPNG(filename);
+        this.draw?.downloadPNG(filename);
     }
 
     getGreyScalePixelArray() {
-        return this.draw.getGreyScalePixelArray();
+        return this.draw?.getGreyScalePixelArray();
     }
 
     getPixelArray() {
-        return this.draw.getPixelArray();
+        return this.draw?.getPixelArray();
     }
 
     getDrawing() {
-        return this.draw.getDrawing();
+        return this.draw?.getDrawing();
     }
 }
